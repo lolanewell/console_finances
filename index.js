631a64400e021ee2c2b69f732ab6f64029003688
lolanewell <117766654+lolanewell@users.noreply.github.com>
2023-01-05 21:01:24 +0000

    completed more of the JS

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
-// ['Dec-2009', 0] either assume december is 0 or jan 2010 changes are 0 , 
-
 var finances = [
 ['Jan-2010', 867884],
-['Feb-2010', 984655], // changes  867884 - 984655
+['Feb-2010', 984655], // first logged diffrence = 984655 - 867884
 ['Mar-2010', 322013],
 ['Apr-2010', -69417],
 ['May-2010', 310503],
@@ -87,14 +85,19 @@ var finances = [
 ['Nov-2016', 795914],
 ['Dec-2016', 60988],
 ['Jan-2017', 138230],
-['Feb-2017', 671099] // changes 6710996 - 138230
+['Feb-2017', 671099] // last logged diffrence = 6710996 - 138230
 ];
 
+// The total number of months included in the dataset:
+
 var arrayLength = finances.length
+
 console.log(finances.length);
 
-// There are 86 months 
+// there are 86 months 
 
+
+// The net total amount of Profit/Losses over the entire period:
 var sum = 0
 
 for (var i = 0; i < finances.length; i++) {
@@ -102,101 +105,54 @@ for (var i = 0; i < finances.length; i++) {
 } 
 console.log(sum)
 
-// The total is 38382578
+// The total of all figures in the array is 38382578
 
-var financeChanges = [
-    ['Jan-2010', 867884],
-    ['Feb-2010', 984655], // changes   984655 - 867884 = 116771
-    ['Mar-2010', 322013], // changes   322013 - 984655 = -662642
-    ['Apr-2010', -69417], // changes   -69417 - 322013 = -391430
-    ['May-2010', 310503],
-    ['Jun-2010', 522857],
-    ['Jul-2010', 1033096],
-    ['Aug-2010', 604885],
-    ['Sep-2010', -216386],
-    ['Oct-2010', 477532],
-    ['Nov-2010', 893810],
-    ['Dec-2010', -80353],
-    ['Jan-2011', 779806],
-    ['Feb-2011', -335203],
-    ['Mar-2011', 697845],
-    ['Apr-2011', 793163],
-    ['May-2011', 485070],
-    ['Jun-2011', 584122],
-    ['Jul-2011', 62729],
-    ['Aug-2011', 668179],
-    ['Sep-2011', 899906],
-    ['Oct-2011', 834719],
-    ['Nov-2011', 132003],
-    ['Dec-2011', 309978],
-    ['Jan-2012', -755566],
-    ['Feb-2012', 1170593],
-    ['Mar-2012', 252788],
-    ['Apr-2012', 1151518],
-    ['May-2012', 817256],
-    ['Jun-2012', 570757],
-    ['Jul-2012', 506702],
-    ['Aug-2012', -1022534],
-    ['Sep-2012', 475062],
-    ['Oct-2012', 779976],
-    ['Nov-2012', 144175],
-    ['Dec-2012', 542494],
-    ['Jan-2013', 359333],
-    ['Feb-2013', 321469],
-    ['Mar-2013', 67780],
-    ['Apr-2013', 471435],
-    ['May-2013', 565603],
-    ['Jun-2013', 872480],
-    ['Jul-2013', 789480],
-    ['Aug-2013', 999942],
-    ['Sep-2013', -1196225],
-    ['Oct-2013', 268997],
-    ['Nov-2013', -687986],
-    ['Dec-2013', 1150461],
-    ['Jan-2014', 682458],
-    ['Feb-2014', 617856],
-    ['Mar-2014', 824098],
-    ['Apr-2014', 581943],
-    ['May-2014', 132864],
-    ['Jun-2014', 448062],
-    ['Jul-2014', 689161],
-    ['Aug-2014', 800701],
-    ['Sep-2014', 1166643],
-    ['Oct-2014', 947333],
-    ['Nov-2014', 578668],
-    ['Dec-2014', 988505],
-    ['Jan-2015', 1139715],
-    ['Feb-2015', 1029471],
-    ['Mar-2015', 687533],
-    ['Apr-2015', -524626],
-    ['May-2015', 158620],
-    ['Jun-2015', 87795],
-    ['Jul-2015', 423389],
-    ['Aug-2015', 840723],
-    ['Sep-2015', 568529],
-    ['Oct-2015', 332067],
-    ['Nov-2015', 989499],
-    ['Dec-2015', 778237],
-    ['Jan-2016', 650000],
-    ['Feb-2016', -1100387],
-    ['Mar-2016', -174946],
-    ['Apr-2016', 757143],
-    ['May-2016', 445709],
-    ['Jun-2016', 712961],
-    ['Jul-2016', -1163797],
-    ['Aug-2016', 569899],
-    ['Sep-2016', 768450],
-    ['Oct-2016', 102685],
-    ['Nov-2016', 795914],
-    ['Dec-2016', 60988],
-    ['Jan-2017', 138230],
-    ['Feb-2017', 671099] // changes 6710996 - 138230
-    ];
+
+// The average of the **changes** in Profit/Losses over the entire period:
+
+// First created an array with the profit/loss difference figures between each year
 
 var profitLoss = []
 
-for (var i = 0; i < financeChanges.length-1; i++) {
-    profitLoss.push(financeChanges[i+1][1] - financeChanges[i][1]);
+for (var i = 0; i < finances.length-1; i++) {
+    profitLoss.push(finances[i+1][1] - finances[i][1]);
 } 
 console.log(profitLoss);
 
+// Second 
+
+/* var sum2 = profitLoss.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0);
+  
+  console.log(sum2); */
+
+  var sum2 = 0
+
+  for (var i = 0; i < profitLoss.length; i++) {
+    sum2 += profitLoss[i];
+} 
+console.log(sum)
+
+// sum of the profit/loss difference figures is -196785
+
+
+var avg = (sum2/profitLoss.length) || 0;
+
+console.log(Math.round(avg));
+
+// average of the profit/loss difference figures is -2315 (rounded)
+
+
+var max = profitLoss.reduce((a, b) => Math.max(a, b));
+
+console.log(max);
+
+// the greatest increase in profits is 1926159
+
+
+var min = profitLoss.reduce((a, b) => Math.min(a, b));
+
+console.log(min);
+
+// the greatest decrease in profits is -2196167
